956ee2518ab303653ec7b542e7bd2f0739c5b964
Jeff Kirui <kipkoech_g@yahoo.com>
2023-07-13 09:07:24 -0700

    Adding 6-size.c file

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
-  * main - A program that prints the size of various types on the computer it is compiled and run on
+  * main - A program that prints the size of various types on the computer
   * Return: 0 (Success)
   */
 int main(void)
