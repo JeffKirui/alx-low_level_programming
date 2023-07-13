ddd8b657a8df223e37fa660793cca47bb428139d
Jeff Kirui <kipkoech_g@yahoo.com>
2023-07-13 09:04:48 -0700

    Adding 6-size.c file

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -16,4 +16,6 @@ int main(void)
 	printf("Size of a long int: %lu byte(s)\n", sizeof(c));
 	printf("Size of a long long int: %lu byte(s)\n", sizeof(d));
 	printf("Size of a float: %lu byte(s)\n", sizeof(f));
+
+	return (0);
 }
