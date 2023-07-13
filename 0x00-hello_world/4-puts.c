2e42b151b8b48e85155910361ab351e5c9384ed8
Jeff Kirui <kipkoech_g@yahoo.com>
2023-07-13 08:21:14 -0700

    Adding 4-puts.c file

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+  * main - A program that prints to screen using the puts function
+  * Return: 0 (Success)
+  */
+int main(void)
+{
+	char Mystr[] = "\"Programming is like building a multilingual puzzle";
+
+	puts(Mystr);
+
+	return (0);
+}
