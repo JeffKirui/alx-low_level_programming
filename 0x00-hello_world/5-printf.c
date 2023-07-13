400969491272f562bdfa11422758f99e95e6fb4e
Jeff Kirui <kipkoech_g@yahoo.com>
2023-07-13 08:34:51 -0700

    Adding 5-printf.c file

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+  * main - A program that prints to screen using the function printf
+  * Return: 0 (Success)
+  */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}
