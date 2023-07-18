e471ab1a5f6ec6b2380dfd83704050058a62923c
Jeff Kirui <kipkoech_g@yahoo.com>
2023-07-18 13:29:20 -0700

    Adding 11-print_to_98.c file

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -12,13 +12,13 @@ void print_to_98(int n)
 		while (n > 98)
 			printf("%d, ", n--);
 
-		printf("%d\n", n);		
+		printf("%d\n", n);
 	}
 	else
 	{
 		while (n < 98)
 			printf("%d, ", n++);
 
-		printf("%d\n", n);		
+		printf("%d\n", n);
 	}
 }
