070db4e4d1c19d36cb8c714ce0c8685d1cd84aa8
Jeff Kirui <kipkoech_g@yahoo.com>
2023-07-20 05:41:34 -0700

    Adding 3-print_numbers.c file

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+  * print_numbers - print the numbers from 0 to 9
+  * Description: You can only use _putchar twice
+  */
+void print_numbers(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		_putchar(i + '0');
+		i++;
+	}
+	_putchar('\n');
+}
