fc68717c57e0049ab45c5963f61a9ddf16c3b024
Jeff Kirui <kipkoech_g@yahoo.com>
2023-07-27 07:33:18 -0700

    Adding 100-rot13.c file

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+  * rot13 - rotate characters 13 places in the alphabet
+  * @s: the string
+  * Return: string `s` rotated
+  */
+char *rot13(char *s)
+{
+	int i, j;
+	char data1[] = {"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"};
+	char datarot[] = {"NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm"};
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; j < 52; j++)
+		{
+			if (s[i] == data1[j])
+			{
+				s[i] = datarot[j];
+
+				break;
+			}
+		}
+	}
+
+	return (s);
+}
