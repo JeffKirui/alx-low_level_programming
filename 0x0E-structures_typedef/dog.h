60b6fdb06a39129417dd72ca16260020299c1315
Jeff Kirui <kipkoech_g@yahoo.com>
2023-08-14 21:28:15 +0300

    Task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+  * struct dog - the structure definition of dog
+  * @name: the character string
+  * @age: integer
+  * @owner: character string
+  */
+struct dog
+{
+	char *name;
+
+	float age;
+	char *owner;
+};
+#endif
