d69f87531981c93f6184e75bfc743dd4531f7f2c
Jeff Kirui <kipkoech_g@yahoo.com>
2023-08-28 15:32:00 +0300

    Task 1

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * listint_len - function that returns the number of elements in linked
+ * listint_t list
+ * @h: pointer to start of the list
+ * Return: THe number of elements in the list
+ */
+size_t listint_len(const listint_t *h)
+{
+	int count;
+
+	if (!h)
+	{
+		return (0);
+	}
+
+	for (count = 0; h != NULL; count++)
+	{
+		h = h->next;
+	}
+
+	return (count);
+}
