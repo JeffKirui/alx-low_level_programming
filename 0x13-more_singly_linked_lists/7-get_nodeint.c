0b59308a0f1282970dd595afa0f7907792bdc159
Jeff Kirui <kipkoech_g@yahoo.com>
2023-08-29 10:13:15 +0300

    Modifying line 17

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -15,9 +15,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	while (current)
 	{
 		if (counter == index)
-		{
 			return (current);
-		}
 		counter++;
 		current = current->next;
 	}
