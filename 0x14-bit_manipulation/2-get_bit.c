9c05269dbe80244c7655f69f17433b548d9ca020
Jeff Kirui <kipkoech_g@yahoo.com>
2023-08-31 15:25:14 +0300

    Modifyinh line 13 and 14

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -10,9 +10,9 @@ int get_bit(unsigned long int n, unsigned int index)
 {
 	if (index > sizeof(unsigned long int) * 8)
 		return (-1);
-	
+
 	if ((n >> index) & 1)
-			return (1);
+		return (1);
 	else
 		return (0);
 
